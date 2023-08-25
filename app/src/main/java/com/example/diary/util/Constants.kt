b9009f1807f7a9627a79b1d0ccc987bc8e23bd18
Homiuc Daniel <109294663+homiucdani@users.noreply.github.com>
2023-08-25 12:04:11 +0300

    Ignore Constants

--- app/src/main/java/com/example/diary/util/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.diary.util
-
-object Constants {
-
-    const val APP_ID = "diaryapp-dqyss"
-    const val CLIEND_ID = "977428912752-kgjqau70b6en10jbhpdktm0kt9vioe3m.apps.googleusercontent.com"
-}
